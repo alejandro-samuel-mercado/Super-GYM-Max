686038f2e4ab47fd09481b1853b6d39337866f73
Alejandro Samuel Mercado <124167849+alejandro-samuel-mercado@users.noreply.github.com>
2026-01-20 17:59:09 -0300

    Delete dev directory

--- dev/server/chunks/ssr/_next-internal_server_app_(publicPages)_blog_page_actions_a5f6d656.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/(publicPages)/blog/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%28publicPages%29_blog_page_actions_a5f6d656.js.map
--- dev/static/chunks/app_(publicPages)_page_tsx_49e5781c._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e56a77ca._.js",
-  "static/chunks/node_modules__pnpm_fe8fea83._.js"
-],
-    source: "dynamic"
-});
